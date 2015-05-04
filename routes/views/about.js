4a5c3d2890420c2b8afcbb65111f275bad72ba9a
didymu5 <wu.thomas@gmail.com>
2015-05-03 20:58:45 -0700

    about page up

--- routes/views/about.js
+++ routes/views/about.js
@@ -2,26 +2,26 @@ var keystone = require('keystone'),
 	Enquiry = keystone.list('Enquiry');
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res),
-		locals = res.locals;
-	
-	// Set locals
-	locals.section = 'about';
-	locals.title = 'About - Inheritance Magazine';
 
+		var view = new keystone.View(req, res),
+			locals = res.locals;
 
+		// Set locals
+		locals.section = 'about';
+		locals.title = 'About - Inheritance Magazine';
+		locals.data = {};
 
-	// var q = keystone.list('Post').model.findOne({
-	// 		slug:'about'
-	// 	});
-		
-	// 	q.exec(function(err, result) {
-	// 		console.log(result)
-	// 		locals.data.post = result;
-	// 		next(err);
-	// 	});
-	
-	view.render('about');
-	
+		view.on('init', function(next) {
+				var q = keystone.list('Post').model.findOne({
+					slug: 'about',
+					InfoArticle2: 'static'
+				});
+
+				q.exec(function(err, result) {
+					locals.data.post = result;
+					next(err);
+				});
+			});
+
+view.render('about');
 };
